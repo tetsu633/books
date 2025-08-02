445944a8e54a87c08eedbcae7089323e64f39a6d
tetsu633 <tetsunishimura633@gmail.com>
2025-08-02 12:45:08 +0900

    eslint.config.mjsにおいて、@typescript-eslint/no-empty-functionルールを無効化。

--- apps/frontend/eslint.config.mjs
+++ apps/frontend/eslint.config.mjs
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'url';
 import js from '@eslint/js';
 import { fixupConfigRules } from '@eslint/compat';
 import nx from '@nx/eslint-plugin';
-import baseConfig from '../../eslint.config.mjs';
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
@@ -18,6 +17,7 @@ export default [
   {
     ignores: ['.next/**/*'],
     rules: {
+      '@typescript-eslint/no-empty-function': 'off',
       '@nx/enforce-module-boundaries': [
         'error',
         {
