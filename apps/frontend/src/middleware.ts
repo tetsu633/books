0af28ba471580a3aa35442fbefcd10b55590cc19
tetsu633 <tetsunishimura633@gmail.com>
2025-08-09 09:24:24 +0900

    ミドルウェアを更新し、ログイン済みユーザーがLPページ、ログインページ、サインアップページにアクセスした際にエントリーページへリダイレクトする機能を追加。また、ミドルウェアのマッチャー設定を改善し、特定のパスを除外するように変更。

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -4,8 +4,14 @@ import { NextRequest, NextResponse } from 'next/server';
 export async function middleware(request: NextRequest) {
   const token = await getToken({ req: request });
 
-  // ログイン済みユーザーがLPページにアクセスした場合
-  if (token && request.nextUrl.pathname === '/') {
+  const authPages = ['/login', '/signup'];
+  const lpPages = ['/'];
+
+  // ログイン済みユーザーがLPページ, login, signupページにアクセスした場合
+  if (
+    token &&
+    (lpPages.includes(request.nextUrl.pathname) || authPages.includes(request.nextUrl.pathname))
+  ) {
     return NextResponse.redirect(new URL('/entries', request.url));
   }
 
@@ -13,5 +19,14 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/'],
+  /**
+   * 以下から始まるリクエストパスを除くすべてのパスにマッチ:
+   * - /api(APIルート)
+   * - /_next/static(Next.jsの静的ファイル)
+   * - /_next/image(Next.jsの画像最適化)
+   * - /favicon.ico, sitemap.xml, robots.txt(メタデータファイル)
+   *
+   * これらのパスはミドルウェアの対象外となり、ユーザーがアクセスできるようになる。
+   */
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)'],
 };
