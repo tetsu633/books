028e756fff2fb175ad8a0f69f9c4f597162972e9
tetsu633 <tetsunishimura633@gmail.com>
2025-08-11 00:57:35 +0900

    ミドルウェアでのトークン取得時にNEXTAUTH_SECRETを使用するように修正

--- apps/frontend/src/middleware.ts
+++ apps/frontend/src/middleware.ts
@@ -2,7 +2,7 @@ import { getToken } from 'next-auth/jwt';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function middleware(request: NextRequest) {
-  const token = await getToken({ req: request });
+  const token = await getToken({ req: request, secret: process.env.NEXTAUTH_SECRET });
 
   const authPages = ['/login', '/signup'];
   const lpPages = ['/'];
